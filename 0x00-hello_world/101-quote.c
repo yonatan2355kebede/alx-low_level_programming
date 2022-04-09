5f4b3dcedf25facbf8a0846f6dee8aaeacf9b1ea
yonatan2355kebede <richchurch1122@gmail.com>
2022-04-08 18:35:28 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alwayes 0 (success)
+ */
+int main(voide)
+{
+write(1,"And that piece of art is useful\" - Dora korpar,2025-20-29\n",59);
+return (1);
+}
