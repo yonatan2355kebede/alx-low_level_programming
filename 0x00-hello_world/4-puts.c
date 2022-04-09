e2cf0e41eac21237d421a13c6df949823ceb7ce1
yonatan2355kebede <richchurch1122@gmail.com>
2022-04-08 17:36:22 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- *  *main - entry point
- *   *
- *    *Return: Always 0 (success)
- *    */
+ * main - entry point
+ * 
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"programing is like building a multilingual puzzle");
-       return (0);	
-}
+       return (0);	}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
+#!/bin/bash
 #include<studio.h>
 /**
- * *min - main block
- * *return :
- * */
+ * min - Entry point
+ * return: Alwas 0 (success)
+ */
 int main(viod)
 {
-	print("with proper grammar, but the outcome is a piece of art,\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n);
 	return (0);
 }
