3785be6103573afb69253f0aa983851b55d84232
yonatan2355kebede <richchurch1122@gmail.com>
2022-04-09 18:04:28 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+ /** main - Entery point
+ *
+ *discription print a number and it's last digit along with whether its
+ *last diigit is 0, greater than 5,or less than 6 and not 0.
+ */
+int main(void)
+{
+        int c;
+	for(c=0;c<26;++c)
+		putchar('a'+c);
+            	putchar("\n");
+        return (0);
+}
+
