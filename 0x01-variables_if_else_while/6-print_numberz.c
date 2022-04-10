d0783611759b6162bde1e11fdadaebefe3efbebe
yonatan2355kebede <richchurch1122@gmail.com>
2022-04-09 19:16:56 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+/**
+ * main - Entery point
+ *
+ * Discription: print numbers 0 to 9 on one line
+ * Return: 0
+ */
+int main(void)
+{
+        int i;
+	for(i = 0;i <=9; ++i)
+		putchar('0' + i);
+	putchar('\n');
+        
+        return (0);
+}
+
