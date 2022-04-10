09975b094b0ffd33322a93b02c4704906482563d
yonatan2355kebede <richchurch1122@gmail.com>
2022-04-09 18:38:37 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <studio>
+/**
+ * main - Entery point
+ *
+ *Ddiscription: print lowercase alphbet excluding e and q
+ * Return:0
+ */
+int main(void)
+{
+	char c;
+        for (c ='a';c<='z';++c)
+	{
+        if (c != 'e' && c != 'q')
+		putchar(c)
+}
+        putchar('\n';
+        return (0);
+}
+
