16aefa24f26439c489110086bb4e34e985824350
yonatan2355kebede <richchurch1122@gmail.com>
2022-04-09 19:04:21 -0700

    task 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,8 +4,9 @@
 /**
  * main - Entery point
  *
- * discription: print a number and it's last digit along with whether its
+ * Discription: print a number and it's last digit along with whether its
  * last diigit is 0, greater than 5,or less than 6 and not 0.
+ * Return:0
  */
 int main(void)
 {
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+/**
+ * main - Entery point
+ *
+ * Discription: print a number o t0 9 on one line
+ *Return 0.
+ */
+int main(void)
+{
+        int i;        
+	for (i ='0';i <= '9'; ++i)
+		putchar(i);
+	putchar('\n')
+        return (0);
+}
+
